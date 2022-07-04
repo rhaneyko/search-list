ccc9d996eb8474ea629595e3669a7a4f4737ab5f
rhaneyko <rhaneykohonorio@gmail.com>
2022-07-04 14:26:35 -0400

    React App

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,10 +1,14 @@
 
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 
 import {
     Container,
+    ContainerSearch,
     Title,
     CreateItemList,
+    Inputs,
+    InputAddItem,
+    CreateItem,
     InputSearch,
     AddButton,
     ContainerList,
@@ -12,24 +16,42 @@ import {
     ContainerListItemTitle,
 } from './styles'
 
-const HomePage = () => {
+import {AiOutlinePlusCircle, AiOutlineSearch } from 'react-icons/ai'
+import { isTemplateSpan } from 'typescript';
 
+interface ItemProps {
+    id: number;
+    title: string;
+}
+
+const HomePage = () => {
+    const listItems = [ 'Carne', 'Leite', 'Água', 'Ovo', 'Sabão', 'Trigo', 'Feijão', 'Óleo', 'Arroz' ]
   return (
     <Container>
+        <ContainerSearch>
         <Title>Home Page</Title>
-        <CreateItemList>
-        <InputSearch
-            type="text"
-            placeholder="Search"
-        />
-        <AddButton>add</AddButton>
-        </CreateItemList>
+        <CreateItemList
+        >
+          <Inputs>
+            <InputSearch
+                type="text"
+                placeholder="Search"
 
+            />
+          </Inputs>
+
+        
+        </CreateItemList>
         <ContainerList>
-            <ContainerListItem>
-                <ContainerListItemTitle></ContainerListItemTitle>
-           </ContainerListItem>
+            {listItems.map(item => (
+                <ContainerListItem key={item}>
+                    <ContainerListItemTitle>{item}</ContainerListItemTitle>
+                </ContainerListItem>
+            ))}
+
         </ContainerList>
+
+        </ContainerSearch>
     </Container>
   )
 }
--- src/pages/HomePage/styles.ts
+++ src/pages/HomePage/styles.ts
@@ -6,16 +6,51 @@ export const Container = styled.div`
 
     align-items: center;
     justify-content: center;
-
+    
+    width: 100%;
     height: 100vh;
 
     background-color: ${props => props.theme.colors.background};
 `;
 
+export const ContainerSearch = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    background-color: red;
+
+
+`;
+
 export const Title = styled.h1``;
 
-export const CreateItemList = styled.div``;
+export const CreateItemList = styled.form``;
 
+export const Inputs = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    margin-left: 30px;
+`;
+
+export const InputAddItem = styled.input`
+    width: 200px;
+    height: 30px;
+
+    padding-left: 10px;
+    
+    border: 1px solid #000;
+    border-radius: 5px;
+`;
+export const CreateItem = styled.button`
+    width: 60px;
+    height: 50px;
+
+    margin: 10px;
+    
+
+    border: none;
+`;
 export const InputSearch = styled.input`
     width: 200px;
     height: 30px;
@@ -46,10 +81,25 @@ export const ContainerList = styled.div`
     margin-top: 20px;
 
     width: 300px;
-    height: 300px;
+    height: auto;
 
-    background-color: red;
+    padding: 10px;
+
+    background-color: #CECECE;
 
 `;
-export const ContainerListItem = styled.ul``;
-export const ContainerListItemTitle = styled.li``;
+export const ContainerListItem = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 280px;
+    height: 50px;
+
+    margin: 5px;
+
+    border-radius: 5px;
+
+    background-color: #FFF;
+`;
+export const ContainerListItemTitle = styled.p``;
